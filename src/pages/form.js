520ad4de35412e16dbf1859b580e64f06ebf908a
Una Kravets <una.kravets@gmail.com>
2018-04-21 15:18:02 -0400

    the 80s are a go

--- src/pages/form.js
+++ src/pages/form.js
@@ -24,7 +24,7 @@ export default class Form extends React.Component {
   }
 
   calculateDatecode(value) {
-    let year, month = '';
+    let year, month, country = '';
     let real = false;
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
     ];
@@ -33,6 +33,30 @@ export default class Form extends React.Component {
     const getMonth = (n) => {
       const month = n - 1;
       return monthNames[month]
+    }
+
+    // function to get the country
+    const getCountry = (val) => {
+      let real = false;
+      let code = val.toUpperCase()
+      for (let key in factoryCodes) {
+        if (factoryCodes[key].codes.includes(code)) {
+          real = true
+          return key
+        }
+      }
+      return real
+    }
+
+    const renderResult = () => {
+      if (real) {
+        console.log(`The date code shows a production date of: ${month} ${year}`)
+      } if (country) {
+        console.log(`The item was produced in ${country}`)
+      } if (!real) {
+        console.log('This is not a valid datecode')
+      }
+      this.setState({ hasResult: true });
     }
 
     // if value is only numbers, it was created in the early 1980's (ex. 836)
@@ -49,14 +73,41 @@ export default class Form extends React.Component {
           month = getMonth(value.slice(2, 4))
         }
         real = true
+        renderResult()
       }
     }
 
     // if value is 3-4 numbers followers by two letters, it's late 1980's (ex. 874VX)
     const mid80s = new RegExp('^[0-9]{3,4}[A-Z][A-Z]$')
     if (mid80s.test(value)) {
-      year = 'mid 80s'
-      real = true
+      // check that it's the 80's
+      if (value.slice(0, 1) === '8') {
+        // set year and month
+        year = `19${value.slice(0, 2)}`
+        if (value.length === 5) {
+          month = getMonth(value.slice(2, 3))
+          // check if the country is valid
+          if (getCountry(value.slice(3, 5))) {
+            country = getCountry(value.slice(3, 5))
+          } else {
+            real = false
+            renderResult()
+            return
+          }
+        } else if (value.length === 6) {
+          month = getMonth(value.slice(2, 4))
+          // check if the country is valid
+          if (getCountry(value.slice(4, 6))) {
+            country = getCountry(value.slice(4, 6))
+          } else {
+            real = false
+            renderResult()
+            return
+          }
+        }
+        real = true
+        renderResult()
+      }
     }
 
     // if the value is 2 letters and 4 numbers, it's 1990+ (ex. VI1025 until 2007 and SD2057 after)
@@ -65,13 +116,6 @@ export default class Form extends React.Component {
       year = 'after 80s'
       real = true
     }
-
-    if (real) {
-      console.log(`The date code shows a production date of: ${month} ${year}`)
-    } else {
-      console.log('This is not a valid datecode')
-    }
-    this.setState({hasResult: true});
   }
 
   render() {
