880eada5c0e1e9629c101d03b617e3eee270d200
Una Kravets <una.kravets@gmail.com>
2018-04-21 12:13:15 -0400

    added regex to break things up by year blocks

--- src/pages/form.js
+++ src/pages/form.js
@@ -24,7 +24,35 @@ export default class Form extends React.Component {
   }
 
   calculateDatecode(value) {
-    console.log(value)
+    let year = '';
+    let real = false;
+    // if value is only numbers, it was created in the early 1980's (ex. 836)
+    const early80s = new RegExp('^[0-9]{3}$')
+
+    if (early80s.test(value)) {
+      year = 'early 80s'
+      real = true
+    }
+
+    // if value is 3-4 numbers followers by two letters, it's late 1980's (ex. 874VX)
+    const mid80s = new RegExp('^[0-9]{3,4}[A-Z][A-Z]$')
+    if (mid80s.test(value)) {
+      year = 'mid 80s'
+      real = true
+    }
+
+    // if the value is 2 letters and 4 numbers, it's 1990+ (ex. VI1025 until 2007 and SD2057 after)
+    const after80s = new RegExp('^[A-Z]{2}[0-9]{4}$')
+    if (after80s.test(value)) {
+      year = 'after 80s'
+      real = true
+    }
+
+    if (real) {
+      console.log(`This is a bag from ${year}`)
+    } else {
+      console.log('This is not a valid datecode')
+    }
     this.setState({hasResult: true});
   }
 
